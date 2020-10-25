0d23996bba284738fffe6272a07c03ff66112466
Andriy <refwol131@mail.ru>
2020-10-25 23:13:08 +0200

    Added redux-toolkit

--- src/app/app-reducer.test.ts
+++ src/app/app-reducer.test.ts
@@ -1,24 +1,24 @@
-import {appReducer, InitialStateType, setAppErrorAC, setAppStatusAC} from './app-reducer'
+import {appReducer, setAppErrorAC, setAppStatusAC} from './app-reducer'
 
-let startState: InitialStateType;
+let startState: any;
 
 beforeEach(() => {
     startState = {
-        error: null,
+        error: null as string | null,
         status: 'idle'
     }
 })
 
 test('correct error message should be set', () => {
 
-    const endState = appReducer(startState, setAppErrorAC('some error'))
+    const endState = appReducer(startState, setAppErrorAC({error: 'some error'}))
 
     expect(endState.error).toBe('some error');
 })
 
 test('correct status should be set', () => {
 
-    const endState = appReducer(startState, setAppStatusAC('loading'))
+    const endState = appReducer(startState, setAppStatusAC({status: 'loading'}))
 
     expect(endState.status).toBe('loading');
 })
